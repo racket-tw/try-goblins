95087c3a3dc49b87d89011bc1f3e4251ff1d0dd4
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-11-22 11:42:12 +0800

    make an object

--- main.rkt
+++ main.rkt
@@ -1,3 +1,16 @@
 #lang racket
 (require goblins)
 
+(define a-vat
+  (make-vat))
+
+(define (^friend bcom my-name)
+  (lambda (your-name)
+    (format "Hello ~a, my name is ~a!" your-name my-name)))
+
+(define alice
+  (a-vat 'spawn ^friend "Alice"))
+
+alice
+
+(a-vat 'call alice "Chris")
