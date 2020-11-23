d355711a3057985203b48a271a2344f96c9db084
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2020-11-23 12:36:36 +0800

    dep

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "try-goblins")
-(define deps '("base"))
+(define deps '("base" "goblins"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define pkg-desc "Description Here")
 (define version "0.0")
